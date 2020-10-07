3a1b0a94480fd7a84579c32cd2238fd4fef548ba
rashadhrrs <rashadhrrs@users.noreply.github.com>
2020-10-07 10:59:15 +0700

    Add sidebar

---
+++ src/Feed.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Feed() {
+    return (
+        <div>
+            {/* Header */}
+            <h2>Home</h2>
+
+            {/* Tweet Box */}
+
+            {/* Post */}
+            {/* Post */}
+            {/* Post */}
+            {/* Post */}
+            {/* Post */}
+            {/* Post */}
+            
+        </div>
+    )
+}
+
+export default Feed
